b46a7e5e94f9c41f7f1c9fd96aeb6721055065a7
kabachok-prog <107309093+kabachok-prog@users.noreply.github.com>
2023-06-07 00:07:17 +0300

    Created a slider for the Production page and adapted it

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,1 +1,69 @@
 import { Splide } from '@splidejs/splide'
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  const splideMainLab = new Splide( '#splide-main-laboratories', {
+    type      : 'fade',
+    rewind    : true,
+    pagination: false,
+    arrows    : false,
+  } );
+
+  const splideNavLab = new Splide( '#splide-navigation-laboratories', {
+		gap       : 10,
+    fixedWidth : 172,
+    fixedHeight: 134,
+		rewind    : true,
+		pagination: false,
+    perPage: 2,
+    isNavigation: true,
+  } );
+
+
+  splideMainLab.sync( splideNavLab );
+  splideMainLab.mount();
+  splideNavLab.mount();
+
+  const splideMainDev = new Splide( '#splide-main-develop', {
+    type      : 'fade',
+    rewind    : true,
+    pagination: false,
+    arrows    : false,
+  } );
+
+  const splideNavDev = new Splide( '#splide-navigation-develop', {
+		gap       : 10,
+    fixedWidth : 172,
+    fixedHeight: 134,
+		rewind    : true,
+		pagination: false,
+    perPage: 2,
+    isNavigation: true,
+  } );
+
+
+  splideMainDev.sync( splideNavDev );
+  splideMainDev.mount();
+  splideNavDev.mount();
+
+  const splideMainInnovator = new Splide( '#splide-main-innovator', {
+    type      : 'fade',
+    rewind    : true,
+    pagination: false,
+    arrows    : false,
+  } );
+
+  const splideNavInnovator = new Splide( '#splide-navigation-innovator', {
+		gap       : 10,
+    fixedWidth : 172,
+    fixedHeight: 134,
+		rewind    : true,
+		pagination: false,
+    perPage: 2,
+    isNavigation: true,
+  } );
+
+
+  splideMainInnovator.sync( splideNavInnovator );
+  splideMainInnovator.mount();
+  splideNavInnovator.mount();
+} );
